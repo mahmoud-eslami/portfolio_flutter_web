26b5673c0cdff850bb922af8417aaf6e59bf3f40
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-01-17 18:37:30 +0330

    initalize size config class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/tools/size_config/size_config.dart';
 
 void main() {
   runApp(MyApp());
@@ -7,13 +8,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Mahmoud eslami',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: MainScreen(),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return OrientationBuilder(
+          builder: (context, orientation) {
+            SizeConfig().init(constraints, orientation);
+            return MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Mahmoud eslami',
+              theme: ThemeData(
+                primarySwatch: Colors.amber,
+              ),
+              home: MainScreen(),
+            );
+          },
+        );
+      },
     );
   }
 }
@@ -25,4 +35,15 @@ class MainScreen extends StatelessWidget {
       backgroundColor: Color(0xff3d405b),
     );
   }
+}
+
+class CustomAppBar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => throw UnimplementedError();
 }
