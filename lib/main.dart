5e1ed573b01b535c183648fca7ce55f2a315d718
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-01-17 18:30:01 +0330

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mahmoud eslami',
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+      ),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff3d405b),
+    );
+  }
+}
