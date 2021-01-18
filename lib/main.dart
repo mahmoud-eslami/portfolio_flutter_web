148ffae578a967bbfb42c3c2f0d5bc9b62039956
mahmoud-eslami <eslamimahmoud98@gmail.com>
2021-01-18 13:18:45 +0330

    comfort font added

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
               title: 'Mahmoud eslami',
               theme: ThemeData(
                 primarySwatch: Colors.amber,
+                fontFamily: "Comfort",
               ),
               home: MainScreenBrowser(),
             );
